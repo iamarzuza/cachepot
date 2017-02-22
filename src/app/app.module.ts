34fd898967e0cbcfc3f9b0900d8a046d46915e89
Iamar Zuza de Araújo <iamarzuza@gmail.com>
2017-02-22 16:40:50 -0300

    Aplicação em branco com duas Tabs cada uma chamando uma página

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,15 @@ import { NgModule, ErrorHandler } from '@angular/core';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { ProdutoPage } from '../pages/produto/produto';
+import { CategoriaPage } from '../pages/categoria/categoria';
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,
+    ProdutoPage,
+    CategoriaPage
   ],
   imports: [
     IonicModule.forRoot(MyApp)
@@ -14,7 +18,9 @@ import { HomePage } from '../pages/home/home';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    HomePage,
+    ProdutoPage,
+    CategoriaPage
   ],
   providers: [{provide: ErrorHandler, useClass: IonicErrorHandler}]
 })
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 
 import { NavController } from 'ionic-angular';
+import { ProdutoPage } from '../produto/produto'
+import { CategoriaPage } from '../categoria/categoria'
 
 @Component({
   selector: 'page-home',
@@ -8,8 +10,12 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
+  tabProduto : any;
+  tabCategoria : any;
+
   constructor(public navCtrl: NavController) {
-    
+      this.tabProduto = ProdutoPage;
+      this.tabCategoria = CategoriaPage;
   }
 
 }
